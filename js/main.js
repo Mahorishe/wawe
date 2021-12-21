626aebda911c8c971660c2be8d1832da18a90ef0
Mahorishe <62537782+Mahorishe@users.noreply.github.com>
2021-12-21 16:55:03 +0300

    Initial commit
    Full responsive site

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+
+  $(".menu a, .logo").on("click", function(event){
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
+  
+  $('.slider__inner').slick({
+    dots: true,
+    arrows: false
+  });
+
+  $('.menu__btn, .menu a').on('click', function(){
+    $('.header__top-inner').toggleClass('header__top-inner--active')
+  });
+
+
+  var mixer = mixitup('.gallery__items');
+});
